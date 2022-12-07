1972a0b2dc9b8a5a71121a778fbd2dc2d0acfca6
Héber Abreu <heberreys@gmail.com>
2022-12-06 22:36:13 -0300

    disable worker

--- IlluminareToys.Application/Workers/SyncProductsWorker.cs
+++ IlluminareToys.Application/Workers/SyncProductsWorker.cs
@@ -1,9 +1,8 @@
 ﻿using IlluminareToys.Domain.UseCases;
-using Microsoft.Extensions.Hosting;
 
 namespace IlluminareToys.Application.Workers
 {
-    public class SyncProductsWorker : BackgroundService
+    public class SyncProductsWorker //: BackgroundService
     {
         private readonly ISyncProductsUseCase _syncProductsUseCase;
         //private readonly TimeSpan _interval = TimeSpan.FromHours(3);
@@ -14,13 +13,13 @@ namespace IlluminareToys.Application.Workers
             _syncProductsUseCase = syncProductsUseCase;
         }
 
-        protected override async Task ExecuteAsync(CancellationToken cancellationToken)
-        {
-            //while (!cancellationToken.IsCancellationRequested)
-            //{
-            //    await _syncProductsUseCase.ExecuteAsync(cancellationToken);
-            //    await Task.Delay(_interval, cancellationToken);
-            //}
-        }
+        //protected override async Task ExecuteAsync(CancellationToken cancellationToken)
+        //{
+        //    //while (!cancellationToken.IsCancellationRequested)
+        //    //{
+        //    //    await _syncProductsUseCase.ExecuteAsync(cancellationToken);
+        //    //    await Task.Delay(_interval, cancellationToken);
+        //    //}
+        //}
     }
 }
