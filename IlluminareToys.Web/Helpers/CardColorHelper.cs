63efd7e0fbdc5a79662646b41f2b82e8a7719bd4
Héber Abreu <heberreys@gmail.com>
2023-01-07 17:42:11 -0300

    CHANGES

---
+++ IlluminareToys.Web/Helpers/CardColorHelper.cs
@@ -0,0 +1,29 @@
+﻿using IlluminareToys.Domain.Shared.Extensions;
+
+namespace IlluminareToys.Web.Helpers
+{
+    public static class CardColorHelper
+    {
+        public static string PrimaryClass = "button-menu-bg-primary";
+        public static string SecondaryClass = "button-menu-bg-secondary";
+        public static string TertiaryClass = "button-menu-bg-tertiary";
+
+        public static string GetCurrentClass(string prevClass, string currentClass)
+        {
+            if (currentClass.IsNullOrWhiteSpace() && prevClass.IsNullOrWhiteSpace())
+            {
+                return PrimaryClass;
+            }
+            else if (currentClass.Equals(PrimaryClass))
+            {
+                return SecondaryClass;
+            }
+            else if (currentClass.Equals(SecondaryClass))
+            {
+                return TertiaryClass;
+            }
+
+            return PrimaryClass;
+        }
+    }
+}
